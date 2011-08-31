c6ff7ec7407d5e639709f8ef841cf75f0faf4745
Ollie Relph <ollie+github@relph.me>
2011-08-31 10:50:46 +0100

    Adding a Less Css plugin

---
+++ lessc/lessc.bat
@@ -0,0 +1,1 @@
+@C:\lessc\lessc.exe %*
---
+++ lessc/lessc.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin, subprocess, os
+
+class CompileLessOnSave(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+
+        if not view.file_name().endswith('.less'):
+            return
+
+        if os.name == "nt":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        folder_name, file_name = os.path.split(view.file_name())        
+        args = ['lessc', '-m', file_name]        
+        view.window().run_command('exec', {'cmd': args, 'working_dir': folder_name})
+        process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, startupinfo=startupinfo)

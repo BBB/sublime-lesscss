e86099da16790d4be9111f406dd9be6d954202fc
Ollie Relph <ollie+github@relph.me>
2011-11-16 11:18:43 +0000

    Refactor to bundle .exe

--- lessc/lessc.bat
+++
@@ -1,1 +0,0 @@
-@C:\lessc\lessc.exe %*
--- lessc/lessc.py
+++ lessc/lessc.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, subprocess, os
+import sublime, sublime_plugin, os
 
 class CompileLessOnSave(sublime_plugin.EventListener):
     def on_post_save(self, view):
@@ -6,11 +6,11 @@ class CompileLessOnSave(sublime_plugin.EventListener):
         if not view.file_name().endswith('.less'):
             return
 
+        folder_name, file_name = os.path.split(view.file_name()) 
+        args = []
         if os.name == "nt":
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            args = [sublime.packages_path() + '\lessc\windows\lessc.exe', file_name]
+        else:
+            args = ['lessc', file_name]             
 
-        folder_name, file_name = os.path.split(view.file_name())        
-        args = ['lessc', '-m', file_name]        
         view.window().run_command('exec', {'cmd': args, 'working_dir': folder_name})
-        process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, startupinfo=startupinfo)
